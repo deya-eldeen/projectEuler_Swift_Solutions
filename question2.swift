840131d9f2559b85f5148644c5e7e02f9d0df777
Deya Eldeen <3denator@gmail.com>
2016-11-09 16:03:02 +0200

    Update question2.swift

--- question2.swift
+++ question2.swift
@@ -5,7 +5,6 @@
  By starting with 1 and 2, the first 10 terms will be:
  1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
  By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
- Solution : 4613732
 */
 import Foundation
 
